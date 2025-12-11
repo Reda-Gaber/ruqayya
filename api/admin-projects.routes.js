b0ced217e02d7018040c9ceef6f9e5eb7b263662
Reda-Gaber <redagaber.engineer@gmail.com>
2025-12-11 13:19:06 +0200

    Final working version for Vercel

--- api/admin-projects.routes.js
+++ api/admin-projects.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Project = require("../../models/projects.model");
+const Project = require("../models/projects.m");
 
 // جلب كل المشاريع (للصفحة الرئيسية)
 router.get("/projects", async (req, res) => {
